d1db8d8dd8a6c4b80ec6e0c60956cd86fe646fbf
Vinicius <viniciius83@gmail.com>
2021-03-01 20:50:31 -0300

    Primeira Classe

--- src/app/Principal.java
+++ src/app/Principal.java
@@ -1,10 +1,14 @@
 package app;
 
+import tabuleiro.Posicao;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Oi");
+		Posicao pos = new Posicao(3,5);
+		System.out.println(pos);
 	}
 
 }
+	
